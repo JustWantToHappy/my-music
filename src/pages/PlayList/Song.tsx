b74699fba0d4357c4d73593d5ba955107743b4c4
JustWantToHappy <1302755003@qq.com>
2022-08-04 22:11:54 +0800

    歌单样式调整

--- src/pages/PlayList/Song.tsx
+++ src/pages/PlayList/Song.tsx
@@ -1,27 +1,44 @@
 //歌单组件
-import { Space, Table, Tag } from 'antd';
+import { Table } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import styles from "./index.module.css"
+import { transTime } from "../../utils/help"
+import { PlayCircleOutlined } from "@ant-design/icons"
 const columns: ColumnsType<Music.song> = [
     {
         dataIndex: 'index',
         key: 'id',
-        render: text => <li>1</li>,
+        width: 150,
+        render: (text) => {
+            return <span className={styles["song-order"]}>
+                <>{text}</>
+                <PlayCircleOutlined className={styles['song-order-svg']}/>
+            </span>;
+        },
     },
     {
         title: '歌曲标题',
         dataIndex: 'name',
-        key: 'name',
+        key: 'id',
+        width: 200,
+        render: (text) => {
+            return <span className={styles['song-header']}>{text}</span>
+        }
     },
     {
         title: '时长',
         dataIndex: "dt",
-        key: "dt"
+        key: "id",
+        width: 150,
+        render: (text) => {
+            return <li>{transTime(text)}</li>
+        }
     },
     {
         title: '歌手',
         dataIndex: "ar",
-        key: "ar",
+        key: "id",
+        width: 150,
         render: (singers: Array<Music.song>) => {
             return <div className={styles.song}>
                 {
@@ -39,9 +56,9 @@ const columns: ColumnsType<Music.song> = [
     {
         title: '专辑',
         dataIndex: "al",
-        key: "al",
+        key: "id",
         render: (album) => {
-            return <>{album.name}</>
+            return <span className={styles['song-album']}>{album.name}</span>
         }
     },
 ];
@@ -49,7 +66,12 @@ const columns: ColumnsType<Music.song> = [
 
 const Song = (props: { songs: Array<Music.song> | undefined }) => {
     return (
-        <Table columns={columns} dataSource={props.songs} pagination={false} />
+        <Table
+            columns={columns}
+            dataSource={props.songs}
+            pagination={false}
+            scroll={{ y: 385 }}//实现固定表头
+        />
     )
 };
 
