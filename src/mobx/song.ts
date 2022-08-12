612c74cea13f25370773cc5f45b70771099651fc
JustWantToHappy <1302755003@qq.com>
2022-08-13 00:02:09 +0800

    fix 播放条修正

--- src/mobx/song.ts
+++ src/mobx/song.ts
@@ -1,10 +1,7 @@
 import { observable, computed, action } from "mobx"
-let content: { timer: any, Timer: void, clearTimer: () => void } = {
+let content: { timer: any,  clearTimer: () => void } = {
     //全局定时器
     timer: null,
-    set Timer(timer: any) {
-        this.timer = timer;
-    },
     clearTimer: function () {
         clearInterval(this.timer);
     }
